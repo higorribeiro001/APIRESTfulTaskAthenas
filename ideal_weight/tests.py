08b44a7b6cc8027af8a2ede7a1210450ae235d01
Higor Ribeiro Araujo <higorribeiroaraujo007@gmail.com>
2024-09-10 12:01:42 -0300

    feat: test in ideal_weight

--- ideal_weight/tests.py
+++ ideal_weight/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class UnitaryTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = '/ideal-weight/api/v1/'
+        self.urlPerson = '/person/api/v1/'
+
+    def create_person(self):
+        response = self.client.post(
+            self.urlPerson,
+            {
+                "name": "UserTest",
+                "date_birth": "2000-09-09",
+                "cpf": "12345678900",
+                "sex": "M",
+                "height": 1.60,
+                "weight": 50
+            }
+        )
+        return response 
+
+    def test_return_ideal_weight(self):
+        person_data = self.create_person()
+        response = self.client.get(f"{self.url}{person_data.json()['id']}/")
+
+        self.assertEqual(response.status_code, 200)
+        self.client.delete(f"{self.urlPerson}{person_data.json()['id']}/")
