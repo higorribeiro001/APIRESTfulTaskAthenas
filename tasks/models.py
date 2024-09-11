4b49d29d07e0e64748d56cd54d87c9f6d101b7b2
Higor Ribeiro Araujo <higorribeiroaraujo007@gmail.com>
2024-09-11 01:24:15 -0300

    fix: cpf is unique

--- tasks/models.py
+++ tasks/models.py
@@ -4,7 +4,7 @@ class Person(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=120, null=False, blank=False)
     date_birth = models.DateField(null=False, blank=False)
-    cpf = models.CharField(max_length=14, null=False, blank=False)
+    cpf = models.CharField(max_length=14, null=False, blank=False, unique=True)
     sex = models.CharField(max_length=1, null=False, blank=False)
     height = models.FloatField(null=False, blank=False)
     weight = models.FloatField(null=False, blank=False)
